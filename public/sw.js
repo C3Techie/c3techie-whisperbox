7c28c0608e75966a56746eb8bca5ddf6e515b771
Awesome6192 <christianchibuike14@gmail.com>
2026-05-05 01:20:12 +0100

    chore: add branded assets and pwa configuration

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'whisperbox-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json',
+  '/icon-light-32x32.png',
+  '/apple-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
